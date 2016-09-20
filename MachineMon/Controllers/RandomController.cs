c16dc00ffbb19294002ddac0fc963ea199e8bd0a
ashes999 <ashes999@yahoo.com>
2016-09-20 15:07:49 -0400

    Basic ASP.NET MVC5 + Web API solution Added Random (sample) Web API end-point

---
+++ MachineMon/Controllers/RandomController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MachineMon.Controllers
+{
+    public class RandomController : ApiController
+    {
+        private static Random random = new Random();
+
+        // GET: api/Random
+        public IEnumerable<string> Get()
+        {
+            return new string[] { RandomString(8), RandomString(9), RandomString(13), RandomString(3) };
+        }
+
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ MachineMon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MachineMon.Startup))]
+namespace MachineMon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
