e87112667564602be37bb132a28f0a4d6012b1a5
ashes999 <ashes999@yahoo.com>
2016-10-20 16:22:11 -0400

    Web app generates secure key on first run Key regenerates if it's deleted Created new global site config-settings table

---
+++ MachineMon.DataAccess/Migrations/003_CreateConfigSettingsTable.sql
@@ -0,0 +1,4 @@
+﻿create table [ConfigurationSetting] (
+	Setting varchar(128) PRIMARY KEY NOT NULL,
+	Value varchar(255) NOT NULL
+)
--- MachineMon/Global.asax.cs
+++ MachineMon/Global.asax.cs
@@ -9,24 +9,35 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System;
+using System.Web.Configuration;
+using MachineMon.Core.Domain;
+using System.Linq;
 
 namespace MachineMon.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        // Persistent connection to RabbitMQ
         private static IConnection connection;
         private static IModel channel;
 
+        private IGenericRepository repository;
+
         protected void Application_Start()
         {
+            // Boilerplate MVC setup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            this.repository = (IGenericRepository)System.Web.Mvc.DependencyResolver.Current.GetService(typeof(IGenericRepository));
+            this.GenerateSecureKeyIfMissing();
             this.SetupRabbitMqSubscriber();
         }
-
+        
         protected void Application_End()
         {
             channel.Close(200, "Goodbye!");
@@ -35,8 +46,7 @@ namespace MachineMon.Web
 
         private void SetupRabbitMqSubscriber()
         {
-            var repository = (IGenericRepository)System.Web.Mvc.DependencyResolver.Current.GetService(typeof(IGenericRepository));
-
+            // TODO: move this into config: the hostname where RabbitMQ lives
             var factory = new ConnectionFactory() { HostName = "localhost" };
             connection = factory.CreateConnection();
             channel = connection.CreateModel();
@@ -50,6 +60,28 @@ namespace MachineMon.Web
                 processor.Process(eventArgs);
             };
             channel.BasicConsume(queue: RabbitMqMessageProcessor.QueueName, noAck: true, consumer: consumer);
+        }
+
+        /// <summary>
+        /// The first time you run the web application, it generates a random key to use to encrypt machine passwords.
+        /// This is stored in plaintext in web.config. Since MachineMon is a hosted solution, this is probably secure enough for now.
+        /// Note that deleting this will generate a new key, but it will cause decryption to fail for all host passwords.
+        /// </summary>
+        private void GenerateSecureKeyIfMissing()
+        {
+            var config = repository.GetAll<ConfigurationSetting>("SELECT * FROM ConfigurationSetting").SingleOrDefault(c => c.Setting == "SecureKey");
+            if (config == null)
+            {
+                using (var provider = new System.Security.Cryptography.RNGCryptoServiceProvider())
+                {
+                    // Key is missing. Regenerate it.
+                    var bytes = new byte[32]; // 32 characters = 256 bytes, which is the key size for AES
+                    provider.GetBytes(bytes);
+                    var key = Convert.ToBase64String(bytes);
+                    config = new ConfigurationSetting() { Setting = "SecureKey", Value = key };
+                    this.repository.Insert<ConfigurationSetting>(config);
+                }                
+            }
         }
     }
 }
