35ceb1450055bafe79fb615adabed52633963495
ashes999 <ashes999@yahoo.com>
2016-11-04 16:45:36 -0400

    Work in progress: host passwords are encrypted

--- MachineMon.DomainEntities/Services/AesEncryptionService.cs
+++ MachineMon.DomainEntities/Services/AesEncryptionService.cs
@@ -66,6 +66,52 @@ namespace MachineMon.Core.Services
             }
         }
 
+        //////////// DERP: needed to store the IV ... per-encryption, ideally
+        public string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold 
+            // the decrypted text. 
+            string plaintext = null;
+
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption. 
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream 
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+
         /// <summary>
         /// The first time you run the web application, it generates a random key to use to encrypt machine passwords.
         /// This is stored in plaintext in web.config. Since MachineMon is a hosted solution, this is probably secure enough for now.
---
+++ MachineMon.Encryption/AesProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineMon.Encryption
+{
+    public class AesProxy
+    {
+        private readonly SecureString SecretKey;
+
+        public AesProxy(string key)
+        {
+            this.SecretKey = new SecureString();
+            foreach (char c in key)
+            {
+                SecretKey.AppendChar(c);
+            }
+            this.SecretKey.MakeReadOnly();
+        }
+    }
+}
